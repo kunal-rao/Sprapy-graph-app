eee937d47d9ee6809b32420e3b29c66c6efad495
kunal yadav <kunalyadav1601@gmail.com>
2022-06-12 14:51:43 +0530

    apps made charts left

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+import { resize } from '@syncfusion/ej2/maps';
+
+
+const Orders = () => {
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl '>
+      <Header category="page" title="Orders" />
+
+      <GridComponent 
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging
+        allowSorting >
+        <ColumnsDirective>
+         {ordersGrid.map((item , index)=>(
+            <ColumnDirective key={index} {...item} />
+         ) )}
+        </ColumnsDirective>
+        <Inject  services={[Resize, Page, ContextMenu, Filter, ExcelExport, Sort, Edit, PdfExport]}/>
+      </GridComponent>
+    </div>
+  )
+}
+
+export default Orders
